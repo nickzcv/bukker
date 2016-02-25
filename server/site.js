e1d2f2a0f6ab4e9a1e6efc8a56809e9ea598b081
nikolai.zaitsev <nikolai.zaitsev@axamit.com>
2016-02-25 13:27:41 +0300

    cover upload

--- server/site.js
+++ server/site.js
@@ -1,8 +1,20 @@
 var express    = require('express'),
-	minify     = require('html-minifier').minify,
+	router     = express.Router(),
 	bodyparser = require('body-parser'),
 	nodemailer = require('nodemailer'),
-	router     = express.Router();
+	multer = require('multer'),
+	mime = require('mime'),
+	upload = multer({
+		storage: multer.diskStorage({
+			destination: function (req, file, cb) {
+				cb(null, 'covers');
+			},
+			filename: function (req, file, cb) {
+				cb(null, file.fieldname + '-' + Date.now() + '.' + mime.extension(file.mimetype));
+			}
+		})
+	});
+
 
 // Home page.
 /*
@@ -15,12 +27,12 @@ router.get('/', function (req, res) {
 router.get('/', function(req, res) {
 	var db = req.db;
 	var collection = db.get('books');
-	collection.find({},{},function(err, docs){
+	collection.find({},{},function(err, books){
 		if (err) throw err;
 		res.render('home', res.locals.template_data = {
 			layout: 'main',
 			meta_title: 'Bukker2',
-			docs: docs
+			book: books
 		});
 		//res.json(docs);
 	});
@@ -30,20 +42,19 @@ router.use(bodyparser.urlencoded({
 	extended: false
 }));
 
-router.post('/', function(req, res) {
-	console.log(req.body.title);
+router.post('/', upload.single('cover'), function(req, res) {
 	var db = req.db,
 		title = req.body.title,
 		description = req.body.description,
-		author = req.body.author,
-		cover = req.body.cover,
+		author = req.body.author;
+		console.log(req.file.filename);
 		books = db.get('books');
 	books.insert({
 		'title' : title,
 		'description' : description,
 		'author' : author,
-		'cover' : cover
-	}, function (error, doc) {
+		'cover' : req.file.filename
+	}, function (error, curent) {
 		if (error) {
 			res.send("Could not create new book.");
 		} else {
