4cb42da3860ec153bc79988bf18726539410bd63
nikolai.zaitsev <nikolai.zaitsev@axamit.com>
2016-03-03 19:30:36 +0300

    litres parcer to DB

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,12 +1,14 @@
 (function ($) {
 	'use strict';
 
-
 	//cover overlay toggle
 	$(".cover").on('click', function(){
 		$(".cover").removeClass("active");
 		$(this).toggleClass("active");
 
 	})
+
+	//stars
+
 
 }($));
--- server/site.js
+++ server/site.js
@@ -4,6 +4,10 @@
 	nodemailer = require('nodemailer'),
 	getSlug = require('speakingurl'),
 	multer = require('multer'),
+	request = require('request'),
+	fs = require('fs'),
+	path = require('path'),
+	cheerio = require('cheerio'),
 	mime = require('mime'),
 	upload = multer({
 		storage: multer.diskStorage({
@@ -16,12 +20,112 @@
 		})
 	});
 
-// Home page.
-/*
-router.get('/', function (req, res) {
-	res.render('home', res.locals.template_data);
+// litres parser
+
+
+router.get('/litres/', function (req, res, next) {
+	var db = req.db,
+		books = db.get('books');
+
+	//url = req.params.url;
+	url = 'http://www.litres.ru/aleksandr-shumovich/smeshat-no-ne-vzbaltyvat-recepty-organizacii-meropriyatiy/';
+
+	//function to save cover
+	var download = function(uri, filename, callback){
+		request.head(uri, function(err, res, body){
+			console.log('content-type:', res.headers['content-type']);
+			console.log('content-length:', res.headers['content-length']);
+
+			request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+		});
+	};
+
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			var litresRefID = "156223639";
+
+			var book = {
+				title:"",
+				description:"",
+				year:"",
+				authors:"",
+				ganres:"",
+				tags:"",
+				date:"",
+				cover:"",
+				litresid:"",
+				url:""
+			};
+
+			book.title = $('#main-div .book-title').text();
+			book.description = $('#main-div .book_annotation').text();
+			book.year = $('#main-div dd[itemprop=datePublished]').text();
+
+			//authors get all
+			var authors = [];
+			$('#main-div .book-author a').each(function(i, elem) {
+				authors[i] = $(this).text();
+			});
+			book.authors = authors.join(', ');
+
+			//ganres get all
+			var ganres = [];
+			$('#main-div dd a[itemprop=genre]').each(function(i, elem) {
+				ganres[i] = $(this).text();
+			});
+			book.ganres = ganres.join(', ');
+
+			//tags get all
+			var tags = [];
+			$('#main-div dd:nth-child(6)').each(function(i, elem) {
+				tags[i] = $(this).text();
+			});
+			book.tags = tags.join(', ');
+
+			var cover = $('#main-div .bookpage-cover img:nth-child(2)').attr("src");
+			var newName = 'cover-' + Date.now() + path.extname(cover);
+			download(cover, 'covers/'+newName, function(){
+				console.log('done');
+			});
+			book.cover = newName;
+
+			var litresid = $('link[rel=shortlink]').attr("href");
+			book.litresid = litresid.slice(21);
+
+			book.url = getSlug(book.title);
+
+			/* adding to DB */
+
+			books.insert({
+				'title' : book.title,
+				'description' : book.description,
+				'year': book.year,
+				'authors' : book.authors,
+				'ganres': book.ganres,
+				'tags': book.tags,
+				'date': new Date(),
+				'cover' : book.cover,
+				'litresid' : book.litresid,
+				'url' : book.url
+			}, function (error, curent) {
+				if (error) {
+					res.send("Could not create new book.");
+				} else {
+					console.log("Inserted");
+					//res.location('/');
+					//res.redirect('/');
+				}
+
+			});
+
+			console.log(book);
+			next();
+
+		}
+	})
 });
-*/
+
 
 /* GET BOOKS list. */
 router.get('/', function(req, res) {
@@ -62,7 +166,7 @@ router.get('/book/:url', function(req, res) {
 	});
 });
 
-/* ADD BOOK */
+/* ADD BOOK page */
 router.get('/addbook', function (req, res) {
 	res.render('addbook', res.locals.template_data = {
 		layout: 'main',
@@ -75,10 +179,10 @@ router.use(bodyparser.urlencoded({
 	extended: false
 }));
 
-
+/* Adding BOOK to DB */
 router.post('/addbook', upload.single('cover'), function(req, res) {
 	var db = req.db,
-		books = db.get('books');
+		books = db.get('books'),
 		//validation
 		title = req.body.title,
 		description = req.body.description,
@@ -97,7 +201,7 @@ router.post('/addbook', upload.single('cover'), function(req, res) {
 		'description' : description,
 		'author' : author,
 		'year': year,
-		'ganre': ganre,
+		'ganres': ganre,
 		'date': new Date(),
 		'cover' : cover,
 		'url' : url
