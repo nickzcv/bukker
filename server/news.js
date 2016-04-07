3bc48fa8a72c5df7ccba7c287e656b2ba83b5c49
nikolai.zaitsev <nikolai.zaitsev@axamit.com>
2016-04-07 13:21:10 +0300

    add news paggination

--- server/news.js
+++ server/news.js
@@ -8,18 +8,64 @@
 /* news page */
 router.get('/news', function (req, res) {
 	var db = req.db,
-		news = db.get('news');
+		news = db.get('news'),
+		limit = 20, //items per page
+		totalBooks = 0,
+		pageCount = 1;
 
-	news.find({},{sort: {date: -1}},function(err, news){
+	//try to get page N
+	var page =  req.query.page;
+	if(!page){
+		page = 1;
+	}
+	//get sort param
+	var sort =  req.query.sort;
+	if(!sort){
+		sort = {date : -1}
+	}
+
+	news.find({},{},function(err, allNews){
 		if (err) throw err;
-		res.render('news', res.locals.template_data = {
-			layout: 'main',
-			active: { news: true },
-			meta_title: 'Литературные новости и лучшие подборки книг',
-			news: news
-		});
+		if (allNews) {
+			totalNews = allNews.length;
+			pageCount = Math.ceil(totalBooks / limit);
+			if(pageCount<0)pageCount=0;
 
+			var options = {
+				"limit": limit,
+				"skip": limit*(page-1),
+				"sort": sort
+			};
+
+			news.find({},options,function(err, news){
+				if (err) throw err;
+				res.render('news', res.locals.template_data = {
+					layout: 'main',
+					active: { news: true },
+					meta_title: 'Новости и события',
+					meta_total_books: totalNews,
+					pagination: {
+						page: page,
+						pageCount: pageCount
+					},
+					news: news
+				});
+			});
+
+		} else {
+			console.log("error")
+		}
 	});
+
+
+
+
+
+
+
+
+
+
 
 });
 
