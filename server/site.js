302ff60d4a410d7b03532860af56a2f46088a0fb
nikolai.zaitsev <nikolai.zaitsev@axamit.com>
2016-04-19 10:43:38 +0300

    urls upd

--- server/site.js
+++ server/site.js
@@ -106,7 +106,7 @@ router.get('/books', function(req, res) {
 	//books.find({},{},function(err, allBooks){
 		//if (err) throw err;
 		//if (allBooks) {
-			totalBooks = 20355;//allBooks.length;
+			totalBooks = 20797;//allBooks.length;
 			pageCount = Math.ceil(totalBooks / limit);
 
 			var options = {
