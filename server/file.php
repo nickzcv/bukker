e681844678405aa98c4ff926c7cebfc15519026f
nikolai.zaitsev <nikolai.zaitsev@axamit.com>
2016-06-14 13:00:17 +0300

    sape test

--- server/file.php
+++ server/file.php
@@ -1,8 +1,11 @@
 <?php
 
-    echo "One";
-    function my_function($arg1, $arg2){
-        echo "Two";
-        return $arg1 + $arg2;
-    }
+     if (!defined('_SAPE_USER')){
+        define('_SAPE_USER', '69a6b26a2ff6a12aedf147b6659e87d8');
+     }
+     require_once(realpath($_SERVER['DOCUMENT_ROOT'].'/'._SAPE_USER.'/sape.php'));
+     $sape = new SAPE_client();
+
+     echo $sape->return_links($n);
+
 ?>
--- server/site.js
+++ server/site.js
@@ -53,12 +53,7 @@ router.get('/contacts', function(req, res) {
 	execPhp('file.php', function(error, php, outprint){
 		// outprint is now `One'.
 
-		php.my_function(1, 2, function(err, result, output, printed){
-			// result is now `3'
-			// output is now `One'.
-			// printed is now `Two'.
-			console.log(result);
-		});
+		console.log(outprint);
 	});
 });
 
