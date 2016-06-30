cdd6ff31cbd6961acead75b02cd635ddeb6245a3
Nick Z <nickzcv@gmail.com>
2016-06-30 13:30:11 +0300

    update

--- server/site.js
+++ server/site.js
@@ -107,7 +107,7 @@ router.get('/books', function(req, res) {
 	//books.find({},{},function(err, allBooks){
 		//if (err) throw err;
 		//if (allBooks) {
-			totalBooks = 31065;//allBooks.length;
+			totalBooks = 31527;//allBooks.length;
 			pageCount = Math.ceil(totalBooks / limit);
 
 			var options = {
